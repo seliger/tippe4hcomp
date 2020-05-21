38e42686e56124a785290c4bece86b7d72177595
Corey S. Seliger <corey@seligerfamily.org>
2020-05-21 18:25:55 -0400

    Updated publish.sh
    Refactored to use the hugo commands to publish and
    invalidate the CloudFront instance

--- publish.sh
+++ publish.sh
@@ -3,6 +3,4 @@
 HUGO_ENV="production"
 
 hugo
-aws s3 sync public/ s3://tippe4hcomp.org/
-aws cloudfront create-invalidation --distribution-id EZY0MMS5J7SWB --paths "/*"
-
+hugo deploy --invalidateCDN
